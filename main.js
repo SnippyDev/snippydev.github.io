210aa24e50bbf0cd58c2b41548c1e9f8574e7558
SnippyDev <snippydev@gmail.com>
2025-06-12 22:50:27 -0500

    Add "Wallpapers" and "Projects" buttons to Quick Links section in index.html

---
+++ main.js
@@ -0,0 +1,38 @@
+const images = [
+  "images/bg1.jpg",
+  "images/bg2.png",
+  "images/bg3.jpg",
+  "images/bg4.jpg",
+  "images/bg5.jpg"
+];
+
+let index = 0;
+let showingBg1 = true;
+
+const bg1 = document.getElementById('bg1');
+const bg2 = document.getElementById('bg2');
+
+// Set initial image
+bg1.style.backgroundImage = `url('${images[index]}')`;
+bg1.style.opacity = 1;
+
+function changeBackground() {
+  index = (index + 1) % images.length;
+  const nextImage = `url('${images[index]}')`;
+
+  if (showingBg1) {
+    bg2.style.backgroundImage = nextImage;
+    bg2.style.opacity = 1;
+    bg1.style.opacity = 0;
+  } else {
+    bg1.style.backgroundImage = nextImage;
+    bg1.style.opacity = 1;
+    bg2.style.opacity = 0;
+  }
+
+  showingBg1 = !showingBg1;
+}
+
+window.addEventListener('load', () => {
+  setInterval(changeBackground, 5000);
+});
